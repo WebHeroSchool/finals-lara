c45d82dc7a25fb0e068093850e71991cfc6cc67f
Larisa Bulacheva <i_lara94@mail.ru>
2019-03-29 19:49:37 +0300

    gulp concat and uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
-
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
 
 gulp.task('build', function () {
-    gulp.src(['script/index.js']) 
-	    .pipe(babel({
-			presets: ['@babel/env']
+    gulp.src(['script/*.js']) 
+		.pipe(concat('index.js'))
+		.pipe(babel({
+		presets: ['@babel/env']
 		}))
-        .pipe(gulp.dest('build')) 
+		.pipe(uglify())
+        .pipe(gulp.dest('build')); 
 });
