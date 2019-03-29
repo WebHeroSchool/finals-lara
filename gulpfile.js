fc1c87355be2e170d98b91060614bd5e9bab9117
Larisa Bulacheva <i_lara94@mail.ru>
2019-03-29 09:00:29 +0300

    gulp relocation of files

--- gulpfile.js
+++ gulpfile.js
@@ -2,5 +2,5 @@ var gulp = require('gulp');
 
 gulp.task('build', function () {
     gulp.src(['html/index.html', 'script/index.js']) //Выберем файлы по нужному пути
-        .pipe(gulp.dest('new')) //Положим в папку 
+        .pipe(gulp.dest('build')) //Положим в папку 
 });
