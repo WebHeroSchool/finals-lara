cf58857d03e2c0ad4d136b28e4fb708da17c995c
Larisa Bulacheva <i_lara94@mail.ru>
2019-04-03 20:06:37 +0300

    gulp-sourcemaps added

--- gulpfile.js
+++ gulpfile.js
@@ -3,28 +3,29 @@ const babel = require('gulp-babel');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const cssnano = require('gulp-cssnano');
+const sourcemaps = require('gulp-sourcemaps');
 
 
 gulp.task('build-js', function () {
     gulp.src(['script/*.js']) 
-		.pipe(concat('index.js'))
-
-		.pipe(babel({
-		presets: ['@babel/env']
-		}))
-		.pipe(uglify())
+		.pipe(sourcemaps.init())
+			.pipe(concat('index.js'))
+			.pipe(babel({
+			presets: ['@babel/env']
+			}))
+			.pipe(uglify())
+		.pipe(sourcemaps.write())
         .pipe(gulp.dest('build-js')); 
 });
 
 gulp.task('build-css', function () {
     gulp.src(['styles/*.css']) 
-		.pipe(concat('styles.css'))		
-		// .pipe(babel({
-		// presets: ['@babel/env']
-		// }))
-		.pipe(cssnano())
+		.pipe(sourcemaps.init())    
+			.pipe(concat('styles.css'))		
+			.pipe(cssnano())
+		.pipe(sourcemaps.write())
         .pipe(gulp.dest('build-css')); 
 });
 
 
-gulp.task('build', ['build-js','build-css']);
+gulp.task('default', ['build-js','build-css']);
